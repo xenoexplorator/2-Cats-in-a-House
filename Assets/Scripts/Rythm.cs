3379be9ea0ea00434b8991d725d4ace96c07d20e
xenoexplorator <cedric.pinard@gmail.com>
2018-04-21 12:30:07 -0400

    Début du charting pour Euro Pop

--- Assets/Scripts/Rythm.cs
+++ Assets/Scripts/Rythm.cs
@@ -14,6 +14,12 @@ public class Rythm : MonoBehaviour {
 		KeyCode.UpArrow,
 		KeyCode.RightArrow
 	};
+	private Quaternion[] rotations = new[] {
+		Quaternion.AngleAxis(90, Vector3.forward),
+		Quaternion.AngleAxis(180, Vector3.forward),
+		Quaternion.AngleAxis(0, Vector3.forward),
+		Quaternion.AngleAxis(270, Vector3.forward)
+	};
 	private Dictionary<int,char> stepChart;
 
 	void Start() {
@@ -24,16 +30,18 @@ public class Rythm : MonoBehaviour {
 	}
 	
 	void Update() {
+		if (Input.GetKeyDown(KeyCode.Space)) Debug.Log("step:"+tickCount);
 		for (int i = 0; i < 4; i++) {
 			while (incoming[i].Count > 0 && incoming[i].Peek().PositionY >= 6) {
 				Destroy(incoming[i].Dequeue().gameObject);
-				gameState.SpawnMonster();
+				//gameState.SpawnMonster();
 			}
 			if (incoming[i].Count > 0 && Input.GetKeyDown(keys[i])) {
 				var nextStep = incoming[i].Peek();
 				var accuracy = Math.Abs(nextStep.PositionY - transform.position.y);
 				if (accuracy < 0.25) {
 					gameState.Currency += 100;
+					Debug.Log("tick=" + tickCount);
 					Destroy(incoming[i].Dequeue().gameObject);
 				}
 			}
@@ -43,10 +51,14 @@ public class Rythm : MonoBehaviour {
 	void FixedUpdate() {
 		tickCount++;
 		char place;
-		if (stepChart.TryGetValue(tickCount, out place)) {
+		if (stepChart.TryGetValue(tickCount + 100, out place)) {
 			int index = place - '1';
 			var posX = -1.5f + index * 1.0f;
-			var step = Instantiate<Step>(stepPrefab, new Vector3(posX, 0, 0), Quaternion.identity, transform);
+			var step = Instantiate<Step>(
+					stepPrefab,
+					new Vector3(posX, 0, 0),
+					rotations[index],
+					transform);
 			incoming[index].Enqueue(step);
 		}
 	}
