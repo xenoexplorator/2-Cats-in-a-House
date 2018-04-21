84427cbc82d3730991620d5559e9fe993c261330
Boivin92 <boivin.92@gmail.com>
2018-04-21 19:22:57 -0400

    Fonts + intro

---
+++ Assets/StoryScreenManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryScreenManager : MonoBehaviour {
+
+    public FadeFromBlack year;
+    public FadeFromBlack story;
+    public FadeFromBlack title;
+    public FadeFromBlack continueButton;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        year.isStarted = true;
+        if (year == null)
+            story.isStarted = true;
+        if (story == null)
+            title.isStarted = true;
+        if (title == null)
+            continueButton.isStarted = true;
+	}
+}
