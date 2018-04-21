1f65cca7b98c60f803fd86e973a0514a866c2afb
Boivin92 <boivin.92@gmail.com>
2018-04-21 12:24:16 -0400

    Réorganisation

--- Assets/TD/Scripts/MapGenerator.cs
+++ Assets/TD/Scripts/MapGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,23 +10,71 @@ public class MapGenerator : MonoBehaviour {
     public float origin_x = 0;
     public float origin_y = 4;
     public GameObject tile;
+    private bool[,] IsOnPath;
 
-
-	// Use this for initialization
 	void Start () {
-        
+        InitializePath();
         for (int i = 0; i < 9; i++)
         {
             for (int j = 0; j < 9; j++)
             {
-                var position = new Vector3(origin_x + (-(i - j) * distance_x),origin_y + ( -(i + j) * distance_y));
-                GameObject.Instantiate(tile, position, Quaternion.identity);
+                if (!IsOnPath[j, i]) //GAME JAM!!!!
+                {
+                    var position = new Vector3(origin_x + (-(i - j) * distance_x), origin_y + (-(i + j) * distance_y));
+                    GameObject.Instantiate(tile, position, Quaternion.identity);
+                }
             }
         }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private void InitializePath()
+    {
+        IsOnPath = new bool[9,9];
+
+        IsOnPath[4, 0] = true;
+
+        IsOnPath[1, 1] = true;
+        IsOnPath[2, 1] = true;
+        IsOnPath[3, 1] = true;
+        IsOnPath[4, 1] = true;
+
+        IsOnPath[1, 2] = true;
+        IsOnPath[2, 2] = true;
+        IsOnPath[3, 2] = true;
+        IsOnPath[4, 2] = true;
+        IsOnPath[5, 2] = true;
+        IsOnPath[6, 2] = true;
+        IsOnPath[7, 2] = true;
+
+        IsOnPath[5, 3] = true;
+        IsOnPath[6, 3] = true;
+        IsOnPath[7, 3] = true;
+
+        IsOnPath[0, 4] = true;
+        IsOnPath[1, 4] = true;
+        IsOnPath[4, 4] = true;
+        IsOnPath[5, 4] = true;
+        IsOnPath[7, 4] = true;
+        IsOnPath[8, 4] = true;
+
+        IsOnPath[1, 5] = true;
+        IsOnPath[3, 5] = true;
+        IsOnPath[4, 5] = true;
+        IsOnPath[7, 5] = true;
+
+        IsOnPath[1, 6] = true;
+        IsOnPath[3, 6] = true;
+        IsOnPath[4, 6] = true;
+        IsOnPath[7, 6] = true;
+
+        IsOnPath[1, 7] = true;
+        IsOnPath[2, 7] = true;
+        IsOnPath[3, 7] = true;
+        IsOnPath[4, 7] = true;
+        IsOnPath[5, 7] = true;
+        IsOnPath[6, 7] = true;
+        IsOnPath[7, 7] = true;
+
+        IsOnPath[4, 8] = true;
+    }
 }
---
+++ Assets/TD/Scripts/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject ZombieSpawnPoint;
+    public GameObject SeamanSpawnPoint;
+    public GameObject WooferSpawnPoint;
+
+    public PathList ZombiePath;
+    public PathList SeamanPath;
+    public PathList WooferPath;
+
+    public GameObject ZombieMonster;
+    public GameObject SeamanMonster;
+    public GameObject WerewolfMonster;
+
+    public bool IsSpawning = false;
+    private int frameCount = 0;
+    
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	void FixedUpdate () {
+        frameCount++;
+
+	}
+}
