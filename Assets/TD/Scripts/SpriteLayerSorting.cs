a98567b818179f72e720b5b0deafb1b607b23582
ReM <remmarcoux@gmail.com>
2018-04-21 21:23:08 -0400

    Adding sorting layers and sorter --WARNING--

---
+++ Assets/TD/Scripts/SpriteLayerSorting.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public struct SortingMatcher
+{
+    public SpriteRenderer renderer;
+    public int originalLayer;
+
+    public SortingMatcher(SpriteRenderer r, int l)
+    {
+        renderer = r;
+        originalLayer = l;
+    }
+}
+
+public class SpriteLayerSorting : MonoBehaviour
+{
+    private List<SortingMatcher> spritesMatcher = new List<SortingMatcher>();
+
+	private void Start ()
+    {
+        RegisterSpritesAndOrders();
+	}
+	
+	private void Update ()
+    {
+        AutoSortLayers();
+	}
+
+    private void RegisterSpritesAndOrders()
+    {
+        var sprites = GetComponentsInChildren<SpriteRenderer>();
+        for (var i= 0; i < sprites.Length; i++)
+        {
+            spritesMatcher.Add(new SortingMatcher(sprites[i], sprites[i].sortingOrder));
+        }
+    }
+
+    private void AutoSortLayers()
+    {
+        for (var i = 0; i < spritesMatcher.Count; i++)
+        {
+            spritesMatcher[i].renderer.sortingOrder = Mathf.RoundToInt(spritesMatcher[i].originalLayer - transform.position.y * 10); 
+        }
+    }
+}
